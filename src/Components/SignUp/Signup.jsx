bd9a6a5c8744eb0942dca3be057260851941f82e
mohdbinharun56 <arahaman461@gmail.com>
2024-11-10 12:12:30 +0600

    updated form and stylling

--- src/Components/SignUp/Signup.jsx
+++ src/Components/SignUp/Signup.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
-
+import './Signup.css';
 
 const Signup = () => {
-    const [error,setError] = useState("");
+    const [error, setError] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
         if (e.target.username.value === "") {
             console.log("Username is required");
-            setError("Username is required")
+            setError("Username is required!")
         }
         else if (e.target.password.value === '') {
             console.log("password is required");
-            setError("password is required");
+            setError("Password is required!");
         } else if (e.target.phone.value === '') {
             console.log("Phone is required");
-            setError("phone is required");
+            setError("Phone is required!");
         } else {
             setError("");
             console.log(e.target.username.value);
@@ -25,14 +25,14 @@ const Signup = () => {
 
     }
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
+        <div className="w-fit mx-auto">
+            <form onSubmit={handleSubmit} className="signup">
                 <input type="text" name="username" placeholder="Enter Username" />
                 <input type="password" name="password" placeholder="Enter Password" />
                 <input type="text" name="phone" placeholder="Enter Phone" />
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" className="cursor-pointer bg-gray-400 hover:bg-gray-800 hover:text-white duration-500 hover:rounded-none"/>
             </form>
-            <p>{error? error:""}</p>
+            <p className="text-red-600 text-xl font-medium p-3 m-3">{error ? error : ""}</p>
         </div>
     );
 };
